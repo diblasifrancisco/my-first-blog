4f09d9357479a0b42f43203bba0ab2854f40745c
francisco <francisco@eventbrite.com>
2018-03-13 14:11:32 -0300

    add auditlog extension

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,18 +84,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-   # 'default': {
-   #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   #     'NAME': 'djangogirls',
-   #     'USER': 'name',
-   #     'PASSWORD': '',
-   #     'HOST': 'localhost',
-   #     'PORT': '',
-   # }
    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'djangogirls',
+       'USER': 'name',
+       'PASSWORD': '',
+       'HOST': 'localhost',
+       'PORT': '',
    }
+   # 'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
